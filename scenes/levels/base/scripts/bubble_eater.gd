012f64cbb5c8317587b3f478ce2e9ea0e16bb843
Mikolaj Laptop <wasiak.mikolaj1@gmail.com>
2021-07-11 17:25:51 +0200

    Rurka wciągająca bańki

--- scenes/levels/base/scripts/bubble_eater.gd
+++ scenes/levels/base/scripts/bubble_eater.gd
@@ -1,8 +1,27 @@
 extends Sprite
 
-export (NodePath) var path_label
+signal enough_bubbles
 
+export (NodePath) var path_label
+export (int) var required_amount_of_bubbles := 10
+
+onready var remaining : int = required_amount_of_bubbles 
 onready var label : Label = get_node(path_label)
+
+func update_number():
+	remaining -= 1
+	label.text = String(remaining)
+	
+	if remaining == 0:
+		emit_signal("enough_bubbles")
 
 func _ready():
 	pass
+
+func _on_body_entered(body : Node2D):
+	if body.is_in_group("HappyBubble"):
+		update_number()
+	if body.is_class("Bubble"):
+		body.kill()
+	else:
+		body.queue_free()
