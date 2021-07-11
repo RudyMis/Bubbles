f0d24453475405b4256196008822ee0a9de6a813
RudyMis <wasiak.mikolaj1@gmail.com>
2021-07-12 01:44:42 +0200

    Ładna animacja xd

--- scenes/levels/Intro.gd
+++ scenes/levels/Intro.gd
@@ -3,13 +3,8 @@ extends Control
 func _ready():
 	$A1/AnimationPlayer.play("funnyletters")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _STARTDAGAME():
-	get_tree().change_scene("res://scenes/levels/Level1.tscn")
+	SceneChanger.restart_level()
 
 func _on_cr_button_pressed():
 	$A1.visible = false
