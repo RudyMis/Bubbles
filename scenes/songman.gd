896a60514d3499e2668651378eb2c8e01c40795d
creativegamecreation <)>
2021-07-11 22:42:17 +0200

    Level, Songman etc

--- scenes/songman.gd
+++ scenes/songman.gd
@@ -5,15 +5,22 @@ var songfiles
 var current
 var doubleplay
 var playing
+var mytimer
+var forceskip
 
 func _ready():
 	songfiles = [load(songs[0]), load(songs[1]), load(songs[2]), load(songs[3])]
+	mytimer = Timer.new()
+	mytimer.set_wait_time(3)
+	mytimer.set_one_shot(true)
+	self.add_child(mytimer)
 	current = randi() % 4
 	doubleplay = false
 	playing = true
 	current = randi() % 4
 	$song.stream = songfiles[current]
 	$song.volume_db = 1
+	forceskip = false
 	_on_song_finished()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,6 +33,11 @@ func _on_song_finished():
 		while old == current:
 			current = randi() % 4
 		$song.stream = songfiles[current]
+		$Label.text = "..."
+		if !forceskip:
+			mytimer.start()
+			yield(mytimer, "timeout")
+			forceskip = false
 	$song.play()
 	$Label.text = String(current + 1)
 	doubleplay = !doubleplay
@@ -41,4 +53,5 @@ func _on_playpause_toggle(button_pressed):
 
 func _on_next_pressed():
 	doubleplay = true
+	forceskip = true
 	_on_song_finished()
