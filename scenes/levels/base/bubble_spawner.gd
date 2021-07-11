d0e61272d50dbe7438c6acf3dc274ff3bf373792
Mikolaj Laptop <wasiak.mikolaj1@gmail.com>
2021-07-11 13:57:15 +0200

    Guzik na wypuszczaczu baniek ma tekstury

--- scenes/levels/base/bubble_spawner.gd
+++ scenes/levels/base/bubble_spawner.gd
@@ -8,12 +8,12 @@ export (Vector2) var bubble_scale := Vector2(1, 1)
 export (NodePath) var path_label
 export (NodePath) var path_spawn
 export (NodePath) var path_timer
-export (NodePath) var path_anim
+export (NodePath) var path_button
 
 onready var label = get_node(path_label)
 onready var spawn = get_node(path_spawn)
 onready var timer = get_node(path_timer)
-onready var anim = get_node(path_anim)
+onready var button : TextureButton = get_node(path_button)
 
 onready var number_of_bubbles : int = initial_number_of_bubbles
 
@@ -22,14 +22,10 @@ func remove_bubble():
 	label.text = String(number_of_bubbles)
 	if number_of_bubbles <= 0:
 		emit_signal("no_bubbles")
-		$o2button.play("nobub")
-		stop()
-		frame = 0
 
 func _ready():
 	label.text = String(number_of_bubbles)
-	$o2button.play("ok")
-
+	
 func _spawn_bubble():
 	if number_of_bubbles <= 0: return
 
@@ -46,26 +42,6 @@ func _spawn_bubble():
 
 func _on_button_toggled(button_toggle : bool):
 	timer.start() if button_toggle else timer.stop()
-	if button_toggle:
-		if label.text == "0":		# Okropne
-			$o2button.play("nobub")
-		else:
-			$o2button.play("blow")
-		play("plumm")
-	else:
-		stop()
-		if label.text == "0":		# Okropne
-			$o2button.play("nobub")
-		else:
-			$o2button.play("ok")
-		frame = 0
 
-# o2button - kosmetyka
-
-
-
-func _on_Guzik_button_down():
-	$o2button.position = Vector2(0, 6)
-
-func _on_Guzik_button_up():
-	$o2button.position = Vector2(0, 7)
+func _on_no_bubbles():
+	button.disabled = true
