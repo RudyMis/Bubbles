9ede8a544e45e5eab306f949091fe0333b006c3c
Mikolaj Laptop <wasiak.mikolaj1@gmail.com>
2021-07-11 10:42:30 +0200

    Coś co samo robi bańki

---
+++ scenes/bubbles/bubble_scanner.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+signal bubble_ready
+
+export (PackedScene) var ps_bubble
+export (NodePath) var path_tween
+
+onready var tween = get_node(path_tween)
+
+func stop_growing():
+	if ps_bubble == null: return
+	var bubble = ps_bubble.instance()
+	bubble.position = position
+	bubble.rescale(scale)
+	get_parent().add_child(bubble)
+	emit_signal("bubble_ready", bubble)
+	call_deferred("queue_free")
+
+func start_growing(grow_time : float, max_scale : Vector2):
+	if tween == null: return
+	
+	tween.remove_all()
+	tween.interpolate_property(
+		self,
+		"scale",
+		Vector2(0.25, 0.25),
+		max_scale,
+		grow_time,
+		Tween.TRANS_SINE,
+		Tween.EASE_OUT
+	)
+	tween.start()
+
+func _ready():
+	pass
+
+func _tween_all_completed():
+	stop_growing()
--- scenes/levels/base/create_bubbles.gd
+++ scenes/levels/base/create_bubbles.gd
@@ -4,50 +4,11 @@ export (PackedScene) var ps_bubble_scanner
 export (PackedScene) var ps_bubble
 export (float) var joint_softness := 1.2
 export (float) var hold_time := 2
+export (Vector2) var max_scale := Vector2(1.5, 1.5)
 
-var tween = null
-var new_bubble_scanner = null
+var bubble_scanner = null
 
 var bubbles_to_connect := Array()
-
-func _create_bubble_scanner():
-	new_bubble_scanner = ps_bubble_scanner.instance()
-	new_bubble_scanner.connect("body_entered", self, "_on_bubble_collision")
-	new_bubble_scanner.connect("body_exited", self, "_on_bubble_exited")
-	get_parent().add_child(new_bubble_scanner)
-	new_bubble_scanner.set_position(get_viewport().get_mouse_position())
-	
-	tween = Tween.new()
-	add_child(tween)
-	tween.connect("tween_all_completed", self, "_release_bubble")
-	tween.interpolate_property(
-		new_bubble_scanner,
-		"scale",
-		Vector2(0.25, 0.25),
-		Vector2(1.5, 1.5),
-		hold_time,
-		Tween.TRANS_SINE,
-		Tween.EASE_OUT
-	)
-	tween.start()
-
-func _release_bubble():
-	
-	if new_bubble_scanner == null: return
-	
-	var new_bubble = ps_bubble.instance()
-	get_parent().add_child(new_bubble)
-	
-	new_bubble.position = new_bubble_scanner.position
-	new_bubble.rescale(new_bubble_scanner.scale)
-	connect_to_bubbles(new_bubble)
-	
-	tween.stop_all()
-	tween.queue_free()
-	tween = null
-	
-	new_bubble_scanner.queue_free()
-	new_bubble_scanner = null
 
 # Łączy się z innymi bańkami w zasięgu
 func connect_to_bubbles(bubble : Bubble):
@@ -61,8 +22,35 @@ func connect_to_bubbles(bubble : Bubble):
 		joint.node_b = joint.get_path_to(another_bubble)
 	bubbles_to_connect.clear()
 
+func disable(value : bool):
+	disabled = value
+	mouse_filter = 2 if value else 0
+
 func _ready():
 	pass # Replace with function body.
+
+func _create_bubble_scanner():
+	bubble_scanner = ps_bubble_scanner.instance()
+	get_parent().add_child(bubble_scanner)
+	
+	bubble_scanner.connect("body_entered", self, "_on_bubble_collision")
+	bubble_scanner.connect("body_exited", self, "_on_bubble_exited")
+	bubble_scanner.connect("bubble_ready", self, "_release_bubble")
+	
+	bubble_scanner.set_position(get_viewport().get_mouse_position())
+	bubble_scanner.start_growing(hold_time, max_scale)
+
+func _release_bubble(bubble):
+	if bubble == null: return
+	get_tree().call_group("BubbleSpawner", "remove_bubble")
+	
+	bubble_scanner = null
+	
+	connect_to_bubbles(bubble)
+
+func _on_button_released():
+	if bubble_scanner == null: return
+	bubble_scanner.stop_growing()
 
 func _on_bubble_collision(body : Node):
 	if body.is_class("Bubble"):
